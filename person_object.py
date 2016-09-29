425bb3a19e1e322750da6269778c4d4353ae8fe7
Robert Dunn <robertdunn220@gmail.com>
2016-09-29 15:59:22 -0400

    Added the num_unique_people_greeted method

--- person_object.py
+++ person_object.py
@@ -1,12 +1,30 @@
 class Person(object):
-    def __init__(self, name, email, phone, friends):
+    def __init__(self, name, email, phone, friends, greeting_count, greet_list, num_of_people_greeted):
         self.name = name
         self.email = email
         self.phone = phone
         self.friends = []
+        self.greeting_count = greeting_count
+        self.greet_list = []
+        self.num_of_people_greeted = 1
+
+    def __repr__(self):
+        return "%s" % (self.name)
 
     def greet(self, other_person):
         print 'Hello %s, I am %s' % (other_person.name, self.name)
+        self.greeting_count += 1
+        self.greet_list.append(other_person.name)
+
+    def num_unique_people_greeted(self):
+        y = 0
+        for x in self.greet_list:
+            if x == self.greet_list[y + 1]:
+                y += 1
+                pass
+            else:
+                self.num_of_people_greeted += 1
+        print self.num_of_people_greeted
 
     def print_contact_info(self):
         print "%s's email: %s, %s's phone number: %s" % (self.name, self.email, self.name, self.phone)
@@ -20,18 +38,15 @@ class Person(object):
     def num_friends(self):
         print "%s has %d friends" % (self.name, len(self.friends))
 
-sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948', '')
-jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456', '')
-john = Person('John', 'papajohns@inthehouse.com', '404-983-PZZA', '')
+sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948', '', 0, '', 1)
+jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456', '', 0, '', 1)
+john = Person('John', 'papajohns@inthehouse.com', '404-983-PZZA', '', 0, '', 1)
 
 sonny.add_friend(jordan)
 sonny.add_friend(john)
 
 sonny.greet(jordan)
-sonny.print_contact_info()
-
-jordan.greet(sonny)
-jordan.print_contact_info()
-
-sonny.num_friends()
-jordan.num_friends()
+sonny.greet(john)
+sonny.greet(jordan)
+sonny.greet(jordan)
+print sonny.num_unique_people_greeted()
